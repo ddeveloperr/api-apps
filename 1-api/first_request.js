62882e96201ce23d59cfbf8e52d9b74e819e69cf
KemalC <ddeveloperr@gmail.com>
2017-07-15 20:40:35 +0000

    Make API request with node.js, request pkg

---
+++ 1-api/first_request.js
@@ -0,0 +1,25 @@
+// Basic
+
+// var request = require('request');
+// request('http://www.google.com', function (error, response, body) {
+//   console.log('error:', error); // Print the error if one occurred
+//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//   console.log('body:', body); // Print the HTML for the Google homepage.
+// });
+
+var request = require('request');
+request('http://www.google.com', function (error, response, body) {
+  
+  if(error) {
+       console.log("Something goes wrong man!");
+  } else {
+      if(response.statusCode = 200) {
+          console.log("Things worked well :)")
+          //console.log(body)
+      }
+  }
+  
+  
+  
+  
+});
