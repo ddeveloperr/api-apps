4faea0596546f33f4eac8220d68cd712f9f0f6b5
KemalC <ddeveloperr@gmail.com>
2017-07-19 22:08:47 +0000

    Display data

--- 3-movie-search-app/app.js
+++ 3-movie-search-app/app.js
@@ -7,11 +7,11 @@ app.set("view engine", "ejs");
 // routes
 app.get("/results", function(req, res){
     // res.send("Results route...")
-    request("http://www.omdbapi.com/?s=guardians+of+the+galaxy&apikey=thewdb",
+    request("http://www.omdbapi.com/?s=california&apikey=thewdb",
              function(error, response, body){
                  if(!error && response.statusCode == 200 )
-                   var results = JSON.parse(body);
-                   res.render("results");
+                   var data = JSON.parse(body);
+                   res.render("results", {data: data});
              });
 });
 
